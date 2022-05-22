556a5c05a7b105a439d2075cfbd5b389de6f8628
Carlos Augusto <cjunior@hotmail.com>
2022-05-21 21:52:36 -0300

    Versao inicial, conectando com o banco

---
+++ src/test/java/br/com/apipesquisa/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apipesquisa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
